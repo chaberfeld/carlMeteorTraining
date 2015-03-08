84dc468e96731e647ad69db97f47e5f40fa49238
Daniel Leitao <dpcleitao@gmail.com>
2015-03-08 19:39:53 -0400

    Initial

---
+++ server/mainServer.js
@@ -0,0 +1,25 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+Answers = new Meteor.Collection("answers");
+
+Meteor.methods({
+  addAnswer : function(answerText){
+    console.log('Adding Answer ...');
+    var answerId = Answers.insert({
+      'answerText' : answerText,
+      'submittedOn': new Date()
+    });
+    console.log(answerId)
+    return answerId;
+  },
+  incrementYesVotes : function(answerID){
+  console.log(answerID);
+  Answers.update(answerID,{$inc : {'yes':1}});
+},
+incrementNoVotes : function(answerID){
+  console.log(answerID);
+  Answers.update(answerID,{$inc : {'no':1}});
+}
+});// code to run on server at startup
+  });
+}
